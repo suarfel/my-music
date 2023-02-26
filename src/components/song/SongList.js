2da96fd17f3d22729adbede997353751b5afb00d
surafel getahun <surafelgetahun01@gmail.com>
2023-02-26 14:21:46 +0300

    altering the songs list

--- src/components/song/SongList.js
+++ src/components/song/SongList.js
@@ -1,118 +1,119 @@
 import Layout from "../layout/Layout";
 import SongItem from "./SongItem";
 import classes from "./SongList.module.css";
-const SONGS = [
-  {
-    title: "Don't Start Now ",
-    singer: "Dua Lipa",
-    genre: "POP",
-    duration: "5:07",
-  },
-  {
-    title: "Failing",
-    singer: "Travor Daniel",
-    genre: "REGGE",
-    duration: "2:06",
-  },
-  {
-    title: "Heartless",
-    singer: "The Weekend",
-    genre: "POP",
-    duration: "5:45",
-  },
-  {
-    title: "Don't Start Now ",
-    singer: "Dua Lipa",
-    genre: "POP",
-    duration: "5:07",
-  },
-  {
-    title: "Failing",
-    singer: "Travor Daniel",
-    genre: "REGGE",
-    duration: "2:06",
-  },
-  {
-    title: "Heartless",
-    singer: "The Weekend",
-    genre: "POP",
-    duration: "5:45",
-  },
-  {
-    title: "Don't Start Now ",
-    singer: "Dua Lipa",
-    genre: "POP",
-    duration: "5:07",
-  },
-  {
-    title: "Failing",
-    singer: "Travor Daniel",
-    genre: "REGGE",
-    duration: "2:06",
-  },
-  {
-    title: "Heartless",
-    singer: "The Weekend",
-    genre: "POP",
-    duration: "5:45",
-  },
-  {
-    title: "Don't Start Now ",
-    singer: "Dua Lipa",
-    genre: "POP",
-    duration: "5:07",
-  },
-  {
-    title: "Failing",
-    singer: "Travor Daniel",
-    genre: "REGGE",
-    duration: "2:06",
-  },
-  {
-    title: "Heartless",
-    singer: "The Weekend",
-    genre: "POP",
-    duration: "5:45",
-  },
-  {
-    title: "Don't Start Now ",
-    singer: "Dua Lipa",
-    genre: "POP",
-    duration: "5:07",
-  },
-  {
-    title: "Failing",
-    singer: "Travor Daniel",
-    genre: "REGGE",
-    duration: "2:06",
-  },
-  {
-    title: "Heartless",
-    singer: "The Weekend",
-    genre: "POP",
-    duration: "5:45",
-  },
-  {
-    title: "Don't Start Now ",
-    singer: "Dua Lipa",
-    genre: "POP",
-    duration: "5:07",
-  },
-  {
-    title: "Failing",
-    singer: "Travor Daniel",
-    genre: "REGGE",
-    duration: "2:06",
-  },
-  {
-    title: "Heartless",
-    singer: "The Weekend",
-    genre: "POP",
-    duration: "5:45",
-  },
-];
-const SongList = () => {
-  const songs = SONGS.map((song) => {
+// const SONGS = [
+//   {
+//     title: "Don't Start Now ",
+//     singer: "Dua Lipa",
+//     genre: "POP",
+//     duration: "5:07",
+//   },
+//   {
+//     title: "Failing",
+//     singer: "Travor Daniel",
+//     genre: "REGGE",
+//     duration: "2:06",
+//   },
+//   {
+//     title: "Heartless",
+//     singer: "The Weekend",
+//     genre: "POP",
+//     duration: "5:45",
+//   },
+//   {
+//     title: "Don't Start Now ",
+//     singer: "Dua Lipa",
+//     genre: "POP",
+//     duration: "5:07",
+//   },
+//   {
+//     title: "Failing",
+//     singer: "Travor Daniel",
+//     genre: "REGGE",
+//     duration: "2:06",
+//   },
+//   {
+//     title: "Heartless",
+//     singer: "The Weekend",
+//     genre: "POP",
+//     duration: "5:45",
+//   },
+//   {
+//     title: "Don't Start Now ",
+//     singer: "Dua Lipa",
+//     genre: "POP",
+//     duration: "5:07",
+//   },
+//   {
+//     title: "Failing",
+//     singer: "Travor Daniel",
+//     genre: "REGGE",
+//     duration: "2:06",
+//   },
+//   {
+//     title: "Heartless",
+//     singer: "The Weekend",
+//     genre: "POP",
+//     duration: "5:45",
+//   },
+//   {
+//     title: "Don't Start Now ",
+//     singer: "Dua Lipa",
+//     genre: "POP",
+//     duration: "5:07",
+//   },
+//   {
+//     title: "Failing",
+//     singer: "Travor Daniel",
+//     genre: "REGGE",
+//     duration: "2:06",
+//   },
+//   {
+//     title: "Heartless",
+//     singer: "The Weekend",
+//     genre: "POP",
+//     duration: "5:45",
+//   },
+//   {
+//     title: "Don't Start Now ",
+//     singer: "Dua Lipa",
+//     genre: "POP",
+//     duration: "5:07",
+//   },
+//   {
+//     title: "Failing",
+//     singer: "Travor Daniel",
+//     genre: "REGGE",
+//     duration: "2:06",
+//   },
+//   {
+//     title: "Heartless",
+//     singer: "The Weekend",
+//     genre: "POP",
+//     duration: "5:45",
+//   },
+//   {
+//     title: "Don't Start Now ",
+//     singer: "Dua Lipa",
+//     genre: "POP",
+//     duration: "5:07",
+//   },
+//   {
+//     title: "Failing",
+//     singer: "Travor Daniel",
+//     genre: "REGGE",
+//     duration: "2:06",
+//   },
+//   {
+//     title: "Heartless",
+//     singer: "The Weekend",
+//     genre: "POP",
+//     duration: "5:45",
+//   },
+// ];
+const SongList = (props) => {
+  console.log(props.songs);
+  const songs = props.songs.map((song) => {
     return (
       <SongItem
         key={Math.random().toString()}
