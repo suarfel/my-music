b39b2c2553518a43696d11576db8846b165aba50
surafel getahun <surafelgetahun01@gmail.com>
2023-02-26 14:19:36 +0300

    modify songs page

--- src/pages/AllSongs.js
+++ src/pages/AllSongs.js
@@ -1,11 +1,42 @@
-import Layout from "../components/layout/Layout";
-import SongItem from "../components/song/SongItem";
+import { useEffect } from "react";
+import React from "react";
 import SongList from "../components/song/SongList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import { getAllSongs } from "../hooks/api";
+import useHttp from "../hooks/user-http";
+import NoSongsFound from "../components/song/NoSongsFound";
+import Layout from "../components/layout/Layout";
 
 const AllSongs = () => {
-return (
-        <SongList />
-);
-}
+  const {
+    sendRequest,
+    status,
+    data: loadedSongs,
+    error,
+  } = useHttp(getAllSongs, true);
 
-export default AllSongs
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+  console.log(loadedSongs);
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+  if (error) {
+    return <div className="centered focused">{error}</div>;
+  }
+  if (status === "completed" && (!loadedSongs || loadedSongs.length === 0)) {
+        return (<Layout><NoSongsFound /> </Layout>);
+ 
+  }
+  return (
+    <React.Fragment>
+      <SongList songs={loadedSongs} />
+    </React.Fragment>
+  );
+};
+export default AllSongs;
