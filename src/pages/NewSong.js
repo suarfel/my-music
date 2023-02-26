4495685aa00e15b9282909a3b2ec84d1a4d7d008
surafel getahun <surafelgetahun01@gmail.com>
2023-02-26 14:23:30 +0300

    modify the new song page

--- src/pages/NewSong.js
+++ src/pages/NewSong.js
@@ -1,8 +1,24 @@
 import SongForm from "../components/song/SongForm";
+import { useNavigate } from "react-router-dom";
+import useHttp from "../hooks/user-http";
+import { addSong } from "../hooks/api";
+import { useEffect } from "react";
 
 const NewSong = () => {
-    return (
-        <SongForm />
-    );
-}
-export default NewSong
+  const { sendRequest, status } = useHttp(addSong);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (status === "completed") {
+      navigate("/songs");
+    }
+  });
+
+  const addSongHandler = (song) => {
+    sendRequest(song);
+  };
+  return (
+    <SongForm isLoading={status === "pending"} onAddSong={addSongHandler} />
+  );
+};
+export default NewSong;
