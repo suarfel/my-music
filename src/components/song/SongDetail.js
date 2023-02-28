9515eb0990a709b5c3a201c5dcd64a8408f95b81
surafel getahun <surafelgetahun01@gmail.com>
2023-02-28 09:05:30 +0300

    structuring the song detail

---
+++ src/components/song/SongDetail.js
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+import {  useNavigate, useParams } from "react-router-dom";
+import { deleteSong } from "../../hooks/api";
+import useHttp from "../../hooks/user-http";
+import Layout from "../layout/Layout";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./SongDetail.module.css";
+const SongDetail = (props) => {
+  // const params = useParams();
+  const navigate = useNavigate();
+  const {sendRequest,status} = useHttp(deleteSong);
+
+  useEffect(() => {
+    if (status === "completed"){
+      navigate('/songs');
+    }
+  });
+  
+ 
+  return (
+    <Layout>
+      {status == "pending" && <LoadingSpinner />}
+      <div className={classes.songDetail}>
+        <div className={classes.songDetailDesciption}>
+          <div className={classes.songDetailTitle}>
+            <div>Title</div>
+            <div className={classes.detailDescription}>{props.title}</div>
+          </div>
+          <div className={classes.songDetailTitle}>
+            <div>Singer</div>
+            <div className={classes.detailDescription}>{props.singer}</div>
+          </div>
+          <div className={classes.songDetailTitle}>
+            <div>Genre</div>
+            <div className={classes.detailDescription}>{props.genre}</div>
+          </div>
+          <div className={classes.songDetailTitle}>
+            <div>Duration</div>
+            <div className={classes.detailDescription}>{props.duration}</div>
+          </div>
+        </div>
+        <div className={classes.deleteButton} >
+           <button id={props.id} onClick={() =>  sendRequest(props.id)}>Delete Song </button>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+export default SongDetail;
