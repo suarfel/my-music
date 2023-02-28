1e722ec70f10e9cd79b66bc6ed3a5e1862906642
surafel getahun <surafelgetahun01@gmail.com>
2023-02-28 09:06:57 +0300

    delete api handler

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -28,7 +28,7 @@ export async function getSingleSong(songId) {
   const data = await response.json();
 
   if (!response.ok) {
-    throw new Error(data.message || 'Could not fetch quote.');
+    throw new Error(data.message || 'Could not fetch song.');
   }
 
   const loadedSong = {
@@ -52,8 +52,19 @@ export async function addSong(songData) {
   const data = await response.json();
 
   if (!response.ok) {
-    throw new Error(data.message || 'Could not create quote.');
+    throw new Error(data.message || 'Could not create song.');
   }
 
+  return null;
+}
+
+export  async function deleteSong(songId) {
+  console.log(true);
+  const response = await fetch(`${FIREBASE_DOMAIN}/songs/${songId}.json`,{method : 'DELETE'});
+  console.log(response);
+  const data  = response.json();
+  if (!response.ok){
+    throw new Error(data.message || 'could not delete song')
+  }
   return null;
 }
